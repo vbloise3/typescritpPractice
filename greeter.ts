60098c5b5dde9c3ff3dbd7d8996b70a5b662ecb6
Vince Bloise <vbloise3@verizon.net>
2016-07-04 15:09:53 -0400

    Added person class

--- greeter.ts
+++ greeter.ts
@@ -1,10 +1,22 @@
 /**
  * Created by vincebloise on 7/4/16.
  */
-function greeter(person) {
-    return "Hello, " + person;
+class Student {
+    fullName: string;
+    constructor(public firstName, public middleInitial, public lastName) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
 }
 
-var user = "Jane User";
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+function greeter(person: Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+var user = new Student("Marin", "E.", "Bloise");
 
 document.body.innerHTML = greeter(user);
