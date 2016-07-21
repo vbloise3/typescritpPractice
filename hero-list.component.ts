90bd233051b285bea03a176960fabc881d575fb7
Vince Bloise <vbloise3@verizon.net>
2016-07-20 21:54:14 -0400

    Added @Component annotation to the hero-list.component

--- hero-list.component.ts
+++ hero-list.component.ts
@@ -4,12 +4,23 @@
 import {Component, OnInit} from '@angular/core';
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
+
+@Component({
+    selector:    'hero-list',
+    templateUrl: 'app/hero-list.component.html',
+    directives:  [HeroDetailComponent],
+    providers:   [HeroService]
+})
+
 export class HeroListComponent implements OnInit {
     heroes: Hero[];
     selectedHero: Hero;
+
     constructor(private service: HeroService) { }
+
     ngOnInit() {
         this.heroes = this.service.getHeroes();
     }
+
     selectHero(hero: Hero) { this.selectedHero = hero; }
 }
